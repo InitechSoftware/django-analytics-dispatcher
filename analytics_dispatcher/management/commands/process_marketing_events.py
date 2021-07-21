5dbdcf9fc0d119fba272cbff77a2fcb9d03a338e
Andrey Chichak <andrey@initech.co.il>
2021-07-21 18:10:10 +0700

    option renamed

--- analytics_dispatcher/management/commands/process_marketing_events.py
+++ analytics_dispatcher/management/commands/process_marketing_events.py
@@ -7,8 +7,8 @@ from analytics_dispatcher.event import process_event_queue
 
 class Command(BaseCommand):
     def add_arguments(self, parser: ArgumentParser):
-        parser.add_argument('--clear', default=False, action='store_true')
+        parser.add_argument('--clean', default=False, action='store_true')
 
     def handle(self, *args, **options):
-        process_event_queue(clear=options['clear'])
+        process_event_queue(clean=options['clean'])
         pass
